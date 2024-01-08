70cceb6097f6010f057dd0885133d5ef0860620a
Pranav Manjunath <pranav.manjunath01@gmail.com>
2024-01-08 09:52:24 -0500

    added JPanels

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
+import javax.swing.*;
+
 public class Main {
+    JFrame mainFrame;
+    JPanel mainName, sort, addCustomer,SSN1, SSN2, Name1, Name2, Checking1, Checking2, Savings1, Savings2,
+    Debt1, Debt2, AddShare, clearShare, rightStockName, stockType, numOfShares,textPreview,addFile,
+    customerName, stocksAndShorts, mainBox, checking1, checking2, changeChecking, savings1, savings2, changesavings, debt1, debt2,
+    changeDebt, resetAccountValues, netValue, updatePositions, changeStockShort, stock, changeShare1, changeShare2,
+    editStocks, leftStockName, value1, value2, changeValue, cap1, cap2, changeCap, resetStockValues, numOfStocksBought,
+    numOfShortsHeld, addStock, stockName1, stockName2, stockvValue1, stockValue2, stockCap1, stockCap2, viewStockHistory;
+
+
     public Main(){
         System.out.println("ELOO");
     }
