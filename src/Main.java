a66a0e14c7ba9ad794d10bd449569ff8ae8f5d07
Pranav Manjunath <pranav.manjunath01@gmail.com>
2024-01-10 09:40:30 -0500

    started graphics

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,92 @@
 import javax.swing.*;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
+import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+
 public class Main extends JPanel{
-    JFrame mainFrame;
-    JPanel mainName, sort, SSN1, SSN2, Name1, Name2, Checking1, Checking2, Savings1, Savings2,
-    Debt1, Debt2, rightStockName, stockType, numOfShares,textPreview,
-    customerName, stocksAndShorts, mainBox, checking1, checking2,  savings1, savings2, changesavings, debt1, debt2,
-    changeDebt, resetAccountValues, netValue, updatePositions, changeStockShort, stock, changeShare1, changeShare2,
-    editStocks, leftStockName, value1, value2, changeValue, cap1, cap2, changeCap, resetStockValues, numOfStocksBought,
-    numOfShortsHeld, addStock, stockName1, stockName2, stockValue1, stockValue2, stockCap1, stockCap2, viewStockHistory;
-
     private final Font ARIAL_30 = new Font("Arial", Font.PLAIN, 30);
-    public Main()
-    {
-        setPreferredSize(new Dimension(600, 480));
-        setBackground(new Color(200, 200, 200));
-        setFont(ARIAL_30);
-    }
-    public void paintComponent(Graphics g)
-    {
-        super.paintComponent(g);
-// Will draw in the set colour until the colour is changed
-        g.setColor(Color.BLUE);
-// Draws a title in the top centre of the screen
-        String title = "Text in the Window";
-        int xPos = (int) ((this.getWidth() - this.getFontMetrics(ARIAL_30)
 
-                .getStringBounds(title, g).getWidth()) / 2);
-
-        g.drawString(title, xPos, 100);
-// Draws some shapes in different colours
-        g.setColor(Color.RED);
-        g.drawRect(150, 125, 200, 100);
-        g.fillRect(50, 150, 50, 100);
-        g.setColor(Color.GREEN);
-        g.fillOval(300,250, 100, 75);
-    }
     public static void main(String[] args)
     {
-// Create a simple JFrame to hold the GraphicsWindow panel
-        JFrame frame = new JFrame("Simple Graphics Window");
-// Create and add this panel to the frame and then launch the frame
-        Main myPanel = new Main();
-        frame.add(myPanel);
-        frame.pack();
+        Border line =  new LineBorder(Color.lightGray, 4);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1050,570);
+        frame.setLayout(new FlowLayout());
+
+        JPanel left = new JPanel();
+        left.setPreferredSize(new Dimension(250,300));
+        left.setBackground(Color.lightGray);
+        left.setBorder(line);
+        left.setLayout(new GridLayout(12,1));
+        frame.add(left);
+
+        JPanel center = new JPanel();
+        center.setPreferredSize(new Dimension(500,300));
+        center.setBackground(Color.lightGray);
+        center.setBorder(line);
+        center.setLayout(new GridLayout(4,1));
+        frame.add(center);
+
+        JPanel right = new JPanel();
+        right.setPreferredSize(new Dimension(250,300));
+        right.setBackground(Color.lightGray);
+        right.setBorder(line);
+        right.setLayout(new GridLayout(12,1));
+        frame.add(right);
+
+        JPanel bottomLeft = new JPanel();
+        bottomLeft.setPreferredSize(new Dimension(250,200));
+        bottomLeft.setBackground(Color.lightGray);
+        bottomLeft.setBorder(line);
+        bottomLeft.setLayout(new GridLayout(2,1));
+        frame.add(bottomLeft);
+
+        JPanel bottomCenter = new JPanel();
+        bottomCenter.setPreferredSize(new Dimension(500,200));
+        bottomCenter.setBackground(Color.lightGray);
+        bottomCenter.setBorder(line);
+        bottomCenter.setLayout(new GridLayout(7,1));
+        frame.add(bottomCenter);
+
+        JPanel bottomRight = new JPanel();
+        bottomRight.setPreferredSize(new Dimension(250,200));
+        bottomRight.setBackground(Color.lightGray);
+        bottomRight.setBorder(line);
+        bottomRight.setLayout(new GridLayout(7,1));
+        frame.add(bottomRight);
+
+
+
+        JComboBox <String> customerSort  = new JComboBox();
+        customerSort.setSize(250,16);
+        customerSort.set
+        customerSort.addItem("Aaron");
+        customerSort.addItem("Kevin");// replace with data
+        left.add(customerSort);
+
+
+        JButton Alex = new JButton();
+        Alex.setSize(250,16);
+        Alex.setText("Add Customer");
+        left.add(Alex);
+
+
+
+
+
+
+
         frame.setVisible(true);
+
+
+    }
+    public void addCustomerSort()
+    {
+
     }
 
 
